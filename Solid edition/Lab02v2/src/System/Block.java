a5718053ca4a1592b65630060028e6f042c8a0cf
K-lil001 <90007227+Khalil002@users.noreply.github.com>
2021-10-30 13:10:21 -0500

    we actually mining now

--- Solid edition/Lab02v2/src/System/Block.java
+++ Solid edition/Lab02v2/src/System/Block.java
@@ -38,7 +38,6 @@ public class Block {
             nonce++;
             hash = calculateHash();
         }
-        System.out.println("Block Mined!!! : " + hash);
     }
 
     public String getHash() {
